c50583822ea5fdc735b6466482d2ae6fbc76116a
Gabriel Greco <110870588+gabrielgreco1@users.noreply.github.com>
2023-03-24 10:22:33 -0300

    Update script.js

--- script.js
+++ script.js
@@ -28,25 +28,34 @@ window.onload = function () {
     }
 }
 
-const btn = document.getElementById("Roda");
-    btn.addEventListener("click", function () {
+const btn2 = document.getElementById("Roda");
+    btn2.addEventListener("click", function () {
          const fileContent = fileReader.result;
          let validacaoHtml = "";
-             const lines = fileContent.split(/\r?\n/);
-              for (let i = 2; i < lines.length; i += 2) {
+           const lines = fileContent.split(/\r?\n/);
+           validacaoHtml += '<pre class = "titulo">' + "          CNPJ         "  +  "|                SITUAÇÃO" + '</pre>';
+            for (let i = 2; i < lines.length; i += 2) {
                const line1 = lines[i];
-                 if (line1 && line1.length >= 218) {
-                 const interval1 = line1.substring(203, 218).trim();
-               const line2 = lines[i + 1];
-             if (line2 && line2.length >= 160) {
-          const interval2 = line2.substring(127, 160).trim();
-        const condicao = (interval1 != interval2) ? 'Errado' : 'Correto';
-     validacaoHtml += interval1 + " " + interval2 + " " + condicao + "<br>";
+                     if (line1 && line1.length >= 218) {
+                     const interval1 = line1.substring(203, 218);
+                     const line2 = lines[i + 1];
+                         if (line2 && line2.length >= 160) {
+                         const interval2 = line2.substring(127, 142);
+                         let condicao = (interval1 != interval2) ? 'Errado' : 'Correto';
+                                if (condicao != 'Correto'){
+                                    if (!isNaN(interval2) && !interval2.includes("+")) {
+                                        if (/^[^a-zA-Z0-9]+$/.test(interval2)) {
+                                         condicao = " | Não tem chave pix cadastrada no TOTVS";
+                                         validacaoHtml += '<span class = "CNPJ1">' + interval1 + '</span>' + " " + '<span class = "condicao">'+ condicao + '</span>'+"<br>";
+                                        }
+                                    } 
+                                         
+                                }
+                        }
+                    }
             }
             
-                 }
-              }
-    const validacao = document.getElementById("validacao");
-    validacao.innerHTML = validacaoHtml;
-    });
+                   const validacao = document.getElementById("validacao");
+                   validacao.innerHTML = validacaoHtml;
+     });
 
